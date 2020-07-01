e6c748b76fc28ed74e6c643b9505a2e451465987
Lenamphy <lennart.dubbelb@gmail.com>
2020-07-01 14:02:24 +0200

    Added miscellaneous and dungeon items to item tracker
    - Added miscellaneous and dungeon items
    - Toggle grayscale is now 80% to see the difference between deeds and fairies
    - Gave a default background color to item tracker to hide any pixels that are off
    * Current minimum screen length is 700px

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,6 +1,6 @@
 $(".sprite").click(function () {
     if ($(this).css('opacity') == '1') {
-        $(this).css({"opacity": "0.3", "filter": "grayscale(100%)"});
+        $(this).css({"opacity": "0.3", "filter": "grayscale(80%)"});
     } else {
         $(this).css({"opacity": "1", "filter": "grayscale(0%)"});
     }
